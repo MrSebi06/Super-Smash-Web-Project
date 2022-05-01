9196aff0e595a587619534171bc3168b45b8e1c0
Sebi <mihneasebastian06@gmail.com>
2022-05-01 21:30:59 +0200

    Tic Tac Toe

---
+++ TIcTacToe.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Tic Tac Toe</title>
+        <link type="text/css" rel="stylesheet" href="Style/TicTacToe.css">
+    </head>
+    <body>
+        <h1>Tic Tac Toe</h1>
+        <h2>Press download to play</h2>
+    </body>
+</html>
