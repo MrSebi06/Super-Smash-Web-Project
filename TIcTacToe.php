5bd651d20b57e14b8fd898b148f612febc84c3ec
Sebi <mihneasebastian06@gmail.com>
2022-05-02 12:01:47 +0200

    TicTacToe

--- TIcTacToe.php
+++ TIcTacToe.php
@@ -8,5 +8,8 @@
     <body>
         <h1>Tic Tac Toe</h1>
         <h2>Press download to play</h2>
+        <h3>Rediscover the classic of the classics.</h3>
+        <p>Limitless fun with your friends, family or even with strangers online (soon).<br> 
+        Adjust the terrain at your will, change the colors as you like, your imagination <br>is the only frontier ! </p>
     </body>
 </html>
