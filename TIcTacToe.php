2e32bc6ddce5b387926d42aedaf6ed7315b9daf8
Sebi <mihneasebastian06@gmail.com>
2022-05-02 20:36:29 +0200

    TicTacToe

--- TIcTacToe.php
+++ TIcTacToe.php
@@ -6,10 +6,13 @@
         <link type="text/css" rel="stylesheet" href="Style/TicTacToe.css">
     </head>
     <body>
-        <h1>Tic Tac Toe</h1>
-        <h2>Press download to play</h2>
-        <h3>Rediscover the classic of the classics.</h3>
-        <p>Limitless fun with your friends, family or even with strangers online (soon).<br> 
-        Adjust the terrain at your will, change the colors as you like, your imagination <br>is the only frontier ! </p>
+        <h1 class="titleMain">Tic Tac Toe</h1>
+        <h2 class="titleSub">Press download to play</h2>
+        <section class="desc">
+            <h3>Rediscover the classic of the classics.</h3>
+            <p>Limitless fun with your friends, family or even with strangers online (soon).<br> 
+            Adjust the terrain at your will, change the colors as you like, your imagination <br>is the only frontier ! <br><br><br><br><br><br><br><br>
+            <a class="download" href="">DOWNLOAD</a> </p>
+        </section>
     </body>
 </html>
