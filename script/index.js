7b9c7455586b9ddef2bb71b8e1edf975141e531b
Teddy <truongteddy306@icloud.com>
2022-05-05 18:52:46 +0200

    màj

--- script/index.js
+++ script/index.js
@@ -1,12 +1,22 @@
 let loginEl = document.querySelector(".login-el");
 let blurEl = document.querySelector(".blur-el");
+let createAccountEl = document.querySelector(".createAccount-el");
+let backgroundEl = document.querySelector(".background");
+
 
 function login() {
-  loginEl.style.display = "block";
   blurEl.style.filter = "blur(5px)";
+  loginEl.style.display = "block";
+  createAccountEl.style.display = "none";
 }
 
 function fermer() {
-  loginEl.style.display = "none";
   blurEl.style.filter = "blur(0)";
+  loginEl.style.display = "none";
+  createAccountEl.style.display = "none";
+}
+
+function createAccount(){
+    loginEl.style.display = "none";
+    createAccountEl.style.display = "grid";
 }
