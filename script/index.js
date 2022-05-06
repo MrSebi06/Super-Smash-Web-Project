30d2056a58632cef56412ee2d4dc048d9cd4ac1c
Teddy <truongteddy306@icloud.com>
2022-05-06 19:24:44 +0200

    06/05/22, 7am
    Ajout du mot de passe oublié.

--- script/index.js
+++ script/index.js
@@ -1,8 +1,8 @@
 let loginEl = document.querySelector(".login-el");
 let blurEl = document.querySelector(".blur-el");
 let createAccountEl = document.querySelector(".createAccount-el");
+let passwordAccountEl = document.querySelector(".passwordAccount-el");
 let backgroundEl = document.querySelector(".background");
-
 
 //----------------------BACKGROUND PARALLAXE----------------------//
 let image2El = document.querySelector(".image2");
@@ -10,32 +10,39 @@ let image3El = document.querySelector(".image3");
 let image4El = document.querySelector(".image4");
 let image5El = document.querySelector(".image5");
 
-window.addEventListener('scroll', function(){
-    let value = window.scrollY;
+window.addEventListener("scroll", function () {
+  let value = window.scrollY;
 
-    image2El.style.bottom = value*0.15 + (-4*16) + 'px';
-    image3El.style.top = value*0.5 + (3*16) + 'px';
-    image4El.style.bottom = value*0.1 + (-8*16) + 'px'
-    image5El.style.bottom = value*0.28 + (5*16) + 'px'
-})
+  image2El.style.bottom = value * 0.15 + -4 * 16 + "px";
+  image3El.style.top = value * 0.5 + 3 * 16 + "px";
+  image4El.style.bottom = value * 0.1 + -8 * 16 + "px";
+  image5El.style.bottom = value * 0.28 + 5 * 16 + "px";
+});
 //---------------------------------------------------------------//
 function login() {
   blurEl.style.filter = "blur(5px)";
   loginEl.style.display = "block";
   createAccountEl.style.display = "none";
+  passwordAccountEl.style.display = "none";
 
-  setTimeout(function(){
-    grow(loginEl)
-  }, 500)
+  setTimeout(function () {
+    grow(loginEl);
+  }, 500);
 }
 
 function fermer() {
   blurEl.style.filter = "blur(0)";
   loginEl.style.display = "none";
-  createAccountEl.style.display = "none"
-}   
+  createAccountEl.style.display = "none";
+  passwordAccountEl.style.display = "none";
+}
 
-function createAccount(){
-    loginEl.style.display = "none";
-    createAccountEl.style.display = "grid";
+function createAccount() {
+  loginEl.style.display = "none";
+  createAccountEl.style.display = "grid";
+}
+
+function passwordAccount() {
+  loginEl.style.display = "none";
+  passwordAccountEl.style.display = "grid";
 }
