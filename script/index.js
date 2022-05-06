e320f140ba9228d69188a22e392b836792e538c3
Teddy <truongteddy306@icloud.com>
2022-05-06 18:22:55 +0200

    06/05/22, 6pm
    Ajout de parallaxe en background

--- script/index.js
+++ script/index.js
@@ -4,17 +4,36 @@ let createAccountEl = document.querySelector(".createAccount-el");
 let backgroundEl = document.querySelector(".background");
 
 
+//----------------------BACKGROUND PARALLAXE----------------------//
+let image2El = document.querySelector(".image2");
+let image3El = document.querySelector(".image3");
+let image4El = document.querySelector(".image4");
+let image5El = document.querySelector(".image5");
+
+window.addEventListener('scroll', function(){
+    let value = window.scrollY;
+
+    image2El.style.bottom = value*0.15 + (-4*16) + 'px';
+    image3El.style.top = value*0.5 + (3*16) + 'px';
+    image4El.style.bottom = value*0.1 + (-8*16) + 'px'
+    image5El.style.bottom = value*0.28 + (5*16) + 'px'
+})
+//---------------------------------------------------------------//
 function login() {
   blurEl.style.filter = "blur(5px)";
   loginEl.style.display = "block";
   createAccountEl.style.display = "none";
+
+  setTimeout(function(){
+    grow(loginEl)
+  }, 500)
 }
 
 function fermer() {
   blurEl.style.filter = "blur(0)";
   loginEl.style.display = "none";
-  createAccountEl.style.display = "none";
-}
+  createAccountEl.style.display = "none"
+}   
 
 function createAccount(){
     loginEl.style.display = "none";
