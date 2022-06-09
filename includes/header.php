fa72b8bed562bf216113c35783caf488c13e28c2
Teddy <truongteddy306@icloud.com>
2022-06-09 14:52:24 +0200

    MAJ

--- includes/header.php
+++ includes/header.php
@@ -1,65 +1,56 @@
 <header>
-    <nav class="navbar navbar-expand-lg">
-        <a><img class="logo" src="./img/logo.svg" /></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="navigation collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">HOME</a>
-                </li>
+        <nav class="navbar navbar-expand-lg">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#"><img class="logo" src="./img/logo.svg" /></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="links-container collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav">
+                        <div class="pages-link">
+                            <li class="nav-item">
+                                <a class="nav-link <?php echo ($title == 'Accueil') ? 'active' : ''; ?>" aria-current="page" href="#">HOME</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link" href="#" id="arcadeDropdownMenu" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    ARCADE
+                                </a>
+                                <ul class="gamesDropdownList dropdown-menu" aria-labelledby="arcadeDropdownMenu">
+                                    <li><a class="dropdown-item" href="#">Morpion</a></li>
+                                    <li><a class="dropdown-item" href="#">Snake</a></li>
+                                    <li><a class="dropdown-item" href="#">Pacman</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">LOUNGE</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">EVENTS</a>
+                            </li>
+                        </div>
 
-                <li class="nav-item dropdown">
-                    <a class="nav-link" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        ARCADE </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <a class="dropdown-item" href="#">Morpion</a>
-                        <a class="dropdown-item" href="#">Snake</a>
-                        <a class="dropdown-item" href="#">Pacman</a>
-                    </div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">LOUNGE</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">EVENTS</a>
-                </li>
-                <li class="language-nav-item nav-item dropdown">
-                    <a class="nav-link" href="#" id="navbarDropdownMenuLink2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <i class="fa-solid fa-language"></i>
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink3">
-                        <a class="dropdown-item" href="#">Français</a>
-                        <a class="dropdown-item" href="#">English</a>
-                    </div>
-                </li>
-                <?php echo !isset($_SESSION['email']) ? 
-                
-                '
-                <li class="connexion-nav-item nav-item">
-                <a class="nav-link" onclick="login()">
-                    <i class="fa-solid fa-user-astronaut"></i>
-                </a>
-                </li>
-                ' 
-                
-                : 
-                
-                '<li class="connexion-nav-item nav-item dropdown">
-                    <a class="nav-link" href="#" id="navbarDropdownMenuLink3" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <i class="fa-solid fa-user-astronaut"></i>
-                    </a>
-                    <div class="dropdown-menu-profile dropdown-menu" aria-labelledby="navbarDropdownMenuLink3">
-                        <a class="dropdown-item" href="#">Avatar</a>
-                        <a class="dropdown-item" href="#">Profil</a>
-                        <a class="dropdown-item" href="#">Stats</a>
-                        <a class="dropdown-item" href="verifications/logout.php">Déconnexion</a>
-                    </div>
-                </li>' 
-                ?>
-                
-            </ul>
-        </div>
-    </nav>
-</header>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link" href="#" id="languageDropdownMenu" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <i class="language-link fa-solid fa-language"></i>
+                            </a>
+                            <ul class="languagesDropdownList dropdown-menu" aria-labelledby="languageDropdownMenu">
+                                <li><a class="dropdown-item" href="#">Français</a></li>
+                                <li><a class="dropdown-item" href="#">English</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link" href="#" id="userDropdownMenu" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <i class="user-link fa-solid fa-user-astronaut"></i>
+                            </a>
+                            <ul class="userDropdownList dropdown-menu" aria-labelledby="userDropdownMenu">
+                                <li><a class="dropdown-item" href="#">Avatar</a></li>
+                                <li><a class="dropdown-item" href="#">Profil</a></li>
+                                <li><a class="dropdown-item" href="#">Stats</a></li>
+                                <li><a class="dropdown-item" href="#">Déconnexion</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
