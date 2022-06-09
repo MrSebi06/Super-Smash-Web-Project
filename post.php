a8427047d025fbe48769a0e3b7c8dc6c74c8a03c
Anakaryna <78374876+Anakaryna@users.noreply.github.com>
2022-06-09 16:50:15 +0200

    Mise à jour
    lounge
    post
    profile

--- post.php
+++ post.php
@@ -1,37 +1,15 @@
 <!DOCTYPE html>
 <html>
-<?php $title = 'Lounge';
+<?php $title = 'Profil';
 include("includes/head.php") ?>
   <body>
     <main id="neon_font">
 
     <?php include("includes/header.php");?>
 
-        <section class="banner"></section>
 
 
-        <div class="container-fluid div_centered">
-
-            <div class="row">
-              <div class="col">
-                  <div class="d-flex align-items-center filtre_div">
-                  <div class=" row">
-                      <div class="col-auto">
-                        Nom
-                          <input class="filtre" type="text">
-                      </div>
-                      <div class="col-auto">
-                        Catégorie
-                        <input class="filtre" type="text">
-                      </div>
-                      <div class="col-auto">
-                        Trier
-                        <input class="filtre" type="text">
-                      </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+        <div class="container-fluid div_centered post">
 
           <a href="post.php" ><div class="posts">
             <div class="en_tete">
@@ -104,77 +82,56 @@ include("includes/head.php") ?>
             </div>
           </a>
 
+            <div class="commenter">
+              <div class="row">
+                <div class="col-8">
+                  <input type="text" name="comment" placeholder="Comment">
+                </div>
+                <div class="col-4">
+                  <button type="button" class="btn btn-secondary">Submit</button>
+                </div>
+              </div>
+            </div>
 
-            <a href="post.php" ><div class="posts">
-              <div class="en_tete">
-                <div class="row">
-                  <div class="col">
-                    <div class="d-flex align-items-center justify-content-between row">
-                      <div class="col-auto">
-                        <p>06/02/2021</p>
-                      </div>
-                      <div class="col-auto">
-                        <p>#fanart/pacman/figma</p>
-                      </div>
-                      <div class="col-auto">
-                        <div class="row">
-                          <div class="col-auto">
-                              <svg width="35" height="31" viewBox="0 0 35 31" fill="none" xmlns="http://www.w3.org/2000/svg">
-                              <path d="M8.75 28.7234V13.2234C8.75 12.0007 7.77041 11.0714 6.5625 11.0714H2.1875C0.97959 11.0714 0 12.063 0 13.2234V28.6612C0 29.8839 0.97959 30.8132 2.1875 30.8132H6.5625C7.77246 30.9377 8.75 30.0036 8.75 28.7234ZM35 13.2926C35 11.4589 33.5316 9.97259 31.7188 9.97259H21.7041C23.2606 7.34935 24.0639 4.37598 24.0639 3.32835C24.0625 1.6967 22.7979 0 20.6787 0C16.3632 0 18.8768 5.26931 13.2822 9.79821L12.168 10.6978C11.3613 11.3551 10.9512 12.317 10.9443 13.2857C10.9427 13.2873 10.9443 13.2857 10.9443 13.2857L10.9375 24.3571C10.9375 25.402 11.4237 26.3867 12.25 27.0136L14.5838 28.7843C16.4746 30.225 18.7783 31 21.1436 31H25.1562C26.9691 31 28.4375 29.5144 28.4375 27.68C28.4375 27.4284 28.4047 27.1857 28.3523 26.9499C29.668 26.5161 30.625 25.2844 30.625 23.8036C30.625 23.1706 30.4406 22.585 30.137 22.082C31.6572 21.7969 32.8125 20.4475 32.8125 18.8283C32.8125 17.9634 32.478 17.1829 31.943 16.5919C33.6465 16.4757 35 15.0502 35 13.2926Z" fill="black"/>
-                              </svg>
-                              48
-                          </div>
-                          <div class="col-auto">
-                              <svg width="35" height="31" viewBox="0 0 35 31" fill="none" xmlns="http://www.w3.org/2000/svg">
-                              <path d="M6.5625 0.00276792H2.1875C0.97959 0.00276792 0 0.993661 0 2.15478V17.5925C0 18.8152 0.97959 19.7445 2.1875 19.7445H6.5625C7.77041 19.7445 8.75 18.7529 8.75 17.5925V2.21636C8.75 0.993661 7.77246 0.00276792 6.5625 0.00276792ZM31.9443 14.4067C32.4775 13.8185 32.8125 13.0366 32.8125 12.1717C32.8125 10.5476 31.6593 9.20174 30.1369 8.91319C30.4404 8.41428 30.625 7.82612 30.625 7.13415C30.625 5.65888 29.668 4.42304 28.3521 3.99056C28.4033 3.81411 28.4375 3.57123 28.4375 3.32004C28.4375 1.48565 26.9678 0 25.1562 0H21.1442C18.7783 0 16.4767 0.776384 14.5838 2.2129L12.25 3.98364C11.4229 4.61056 10.9375 5.59799 10.9375 6.58058L10.9448 17.652C10.9448 17.652 10.9432 17.6504 10.9448 17.652C10.9496 18.6201 11.3634 19.5854 12.1692 20.2372L13.2834 21.1388C18.8809 25.7341 16.3652 31 20.6787 31C22.7951 31 24.0604 29.3033 24.0604 27.671C24.0604 26.6226 23.2565 23.6493 21.7007 21.0267H31.7188C33.5316 21.0267 35 19.5411 35 17.7067C35 15.9498 33.6465 14.5243 31.9443 14.4067Z" fill="black"/>
-                              </svg>
-                              3
-                          </div>
-                          <div class="col-auto">
-                              <div class="d-flex align-items-center jus tify-content-end row">
-                                <div class="col-auto">
-                                  <svg width="46" height="29" viewBox="0 0 46 29" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                  <path d="M22.893 14.1859C23.8994 13.0399 25.6053 12.8313 26.8581 13.701L37.0311 20.7635C39.4741 22.4595 38.2258 26.2942 35.2526 26.2271L19.196 25.8648C16.6485 25.8074 15.328 22.8008 17.0094 20.8861L22.893 14.1859Z" fill="black"/>
-                                  <rect width="31" height="26" rx="5" fill="black"/>
-                                  <line x1="8" y1="7" x2="24" y2="7" stroke="white" stroke-width="2" stroke-linecap="round"/>
-                                  <line x1="8" y1="13" x2="24" y2="13" stroke="white" stroke-width="2" stroke-linecap="round"/>
-                                  <line x1="8" y1="19" x2="24" y2="19" stroke="white" stroke-width="2" stroke-linecap="round"/>
-                                  </svg>
-                                  12
-                                </div>
-                              </div>
-                          </div>
-                          <div class="col-auto">
-                              <p>...</p>
-                          </div>
-                      </div>
-                      </div>
+            <div class="comments">
+              <div class="row">
+                <div class="col-12">
+                  <div class="d-flex align-items-center row">
+                    <div class="col-3">
+                      <h1>Minela</h1>
+                      <img src="img/profil_Minela.png">
                     </div>
+
+                    <div class="col-1">
+
+                    </div>
+                    <div class="col-7">
+                      <p>Hello, i really love the colors you used in this fanart. Love u :3</p>
+                    </div>
+                    <div class="col-1">
+
+                    </div>
+
                   </div>
                 </div>
               </div>
-
-              <div class="row">
-                  <div class="img_txt_post">
-                        <div class="row">
-                          <div class="col">
-                            <p>My last fanart for the game named ‘Pac man’</p>
-                          </div>
-                            <p>Made with Figma, I spent like 5 hours on it. Hope you’ll enjoy !
-                            If you like my work you can follow me and don’t hesitate to contact me, I’m open to commissions.</p>
-                        </div>
-                        <div class="col-1">
-
-                        </div>
-                        <div class="col-auto">
-                          <img src="img/Fanart.png">
-                        </div>
-                  </div>
+              <div class="d-flex align-items-center justify-content-end row">
+                <div class="col-auto">
+                    <svg width="35" height="31" viewBox="0 0 35 31" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M8.75 28.7234V13.2234C8.75 12.0007 7.77041 11.0714 6.5625 11.0714H2.1875C0.97959 11.0714 0 12.063 0 13.2234V28.6612C0 29.8839 0.97959 30.8132 2.1875 30.8132H6.5625C7.77246 30.9377 8.75 30.0036 8.75 28.7234ZM35 13.2926C35 11.4589 33.5316 9.97259 31.7188 9.97259H21.7041C23.2606 7.34935 24.0639 4.37598 24.0639 3.32835C24.0625 1.6967 22.7979 0 20.6787 0C16.3632 0 18.8768 5.26931 13.2822 9.79821L12.168 10.6978C11.3613 11.3551 10.9512 12.317 10.9443 13.2857C10.9427 13.2873 10.9443 13.2857 10.9443 13.2857L10.9375 24.3571C10.9375 25.402 11.4237 26.3867 12.25 27.0136L14.5838 28.7843C16.4746 30.225 18.7783 31 21.1436 31H25.1562C26.9691 31 28.4375 29.5144 28.4375 27.68C28.4375 27.4284 28.4047 27.1857 28.3523 26.9499C29.668 26.5161 30.625 25.2844 30.625 23.8036C30.625 23.1706 30.4406 22.585 30.137 22.082C31.6572 21.7969 32.8125 20.4475 32.8125 18.8283C32.8125 17.9634 32.478 17.1829 31.943 16.5919C33.6465 16.4757 35 15.0502 35 13.2926Z" fill="black"/>
+                    </svg>
+                    48
                 </div>
+                <div class="col-auto">
+                    <svg width="35" height="31" viewBox="0 0 35 31" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M6.5625 0.00276792H2.1875C0.97959 0.00276792 0 0.993661 0 2.15478V17.5925C0 18.8152 0.97959 19.7445 2.1875 19.7445H6.5625C7.77041 19.7445 8.75 18.7529 8.75 17.5925V2.21636C8.75 0.993661 7.77246 0.00276792 6.5625 0.00276792ZM31.9443 14.4067C32.4775 13.8185 32.8125 13.0366 32.8125 12.1717C32.8125 10.5476 31.6593 9.20174 30.1369 8.91319C30.4404 8.41428 30.625 7.82612 30.625 7.13415C30.625 5.65888 29.668 4.42304 28.3521 3.99056C28.4033 3.81411 28.4375 3.57123 28.4375 3.32004C28.4375 1.48565 26.9678 0 25.1562 0H21.1442C18.7783 0 16.4767 0.776384 14.5838 2.2129L12.25 3.98364C11.4229 4.61056 10.9375 5.59799 10.9375 6.58058L10.9448 17.652C10.9448 17.652 10.9432 17.6504 10.9448 17.652C10.9496 18.6201 11.3634 19.5854 12.1692 20.2372L13.2834 21.1388C18.8809 25.7341 16.3652 31 20.6787 31C22.7951 31 24.0604 29.3033 24.0604 27.671C24.0604 26.6226 23.2565 23.6493 21.7007 21.0267H31.7188C33.5316 21.0267 35 19.5411 35 17.7067C35 15.9498 33.6465 14.5243 31.9443 14.4067Z" fill="black"/>
+                    </svg>
+                    3
                 </div>
-              </a>
-
+              </div>
             </div>
+
+        </div>
 
       </main>
       <?php include("includes/footer.php") ?>
